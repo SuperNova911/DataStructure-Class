0519a843b01c98691bc92324863d119884d5701e
SuperNova911 <suwhan77@naver.com>
2018-10-10 00:06:03 +0900

    이중 연결 리스트 - 자바

---
+++ Assignment1/DoublyLinkedList_Java/src/DoublylinkedList.java
@@ -0,0 +1,132 @@
+public class DoublylinkedList
+{
+	private int count;
+	private Node head;
+
+	public DoublylinkedList()
+	{
+		count = 0;
+		head = new Node();
+	}
+
+	public DoublylinkedList(DoublylinkedList linkedList)
+	{
+		this();
+		
+		for (int index = 0; index < linkedList.count; index++)
+		{
+			this.Insert(index, linkedList.Retrieve(index));
+		}
+	}
+	
+	public void Insert(int position, int item)
+	{
+		if (position < 0 || count < position)
+			return;
+		
+		if (position == 0)
+		{
+			Node newNode = new Node();
+			newNode.Data = item;
+			newNode.Next = head.Next;
+			
+			if (head.Next != null)
+				head.Next.Prev = newNode;
+
+			head.Next = newNode;
+		}
+		else
+		{
+			Node targetNode = head.Next;
+			for (int index = 0; index < position - 1; index++)
+			{
+				targetNode = targetNode.Next;
+			}
+			
+			Node newNode = new Node();
+			newNode.Data = item;
+			newNode.Prev = targetNode;
+			newNode.Next = targetNode.Next;
+			
+			if (targetNode.Next != null)
+				targetNode.Next.Prev = newNode;
+			
+			targetNode.Next = newNode;
+		}
+		
+		count++;
+	}
+	
+	public void Delete(int position)
+	{
+		if (position < 0 || count <= position)
+			return;
+		
+		Node targetNode = head.Next;
+		if (position == 0)
+		{
+			head.Next = targetNode.Next;
+			
+			if (head.Next != null)
+				head.Next.Prev = null;
+		}
+		else
+		{
+			Node prevNode = targetNode;
+			for (int index = 1; index <= position; index++)
+			{
+				prevNode = targetNode;
+				targetNode = targetNode.Next;
+			}
+			
+			prevNode.Next = targetNode.Next;
+			
+			if (targetNode.Next != null)
+				targetNode.Next.Prev = prevNode;
+		}
+		
+		targetNode = null;
+		count--;
+	}
+	
+	public int Retrieve(int position)
+	{
+		if (position < 0 || count < position)
+			return -1;
+		
+		Node targetNode = head.Next;
+		for (int index = 0; index < position; index++)
+		{
+			targetNode = targetNode.Next;
+		}
+		
+		return targetNode.Data;
+	}
+	
+	public boolean IsEmpty()
+	{
+		if (count < 1)
+			return true;
+		else
+			return false;
+	}
+	
+	public int Length()
+	{
+		return count;
+	}
+
+	private class Node
+	{
+		public int Data;
+		public Node Prev;
+		public Node Next;
+		
+		public Node()
+		{
+			Data = 0;
+			Prev = null;
+			Next = null;
+		}
+	}
+}
---
+++ Assignment1/DoublyLinkedList_Java/src/Main.java
@@ -0,0 +1,41 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		DoublylinkedList list = new DoublylinkedList();				// 생성자
+		DoublylinkedList list2 = new DoublylinkedList(list);		// 비어있는 리스트 복사
+		// list = { }, list2 = { }
+
+		list.Insert(0, 2);			// 맨 처음 (비어있는 상태)
+		list.Insert(1, 4);			// 맨 끝
+		list.Insert(0, 1);			// 맨 처음 (값이 있는 상태)
+		list.Insert(2, 3);			// 중간 (앞 뒤로 값이 있는 상태)
+		// list = { 1, 2, 3, 4 }
+		
+		DoublylinkedList list3 = new DoublylinkedList(list);		// 값이 들어있는 리스트 복사
+		// list3 = { 1, 2, 3, 4 }
+
+		int data = 0;
+		data = list.Retrieve(0);		// 맨 처음 data: 1
+		data = list.Retrieve(1);		// 중간 값 data: 2
+		data = list.Retrieve(3);		// 맨 끝   data: 4
+
+		list.Delete(0);			// 맨 처음 삭제
+		list.Delete(1);			// 중간 값 삭제
+		list.Delete(1);			// 맨 끝 삭제
+		// list = { 2 }
+		
+		boolean isEmpty = false;
+		isEmpty = list.IsEmpty();		// list: { 2 }				isEmpty: False
+		isEmpty = list2.IsEmpty();		// list2: { }				isEmpty: True
+		isEmpty = list3.IsEmpty();		// list3: { 1, 2, 3, 4 }	isEmpty: False
+		
+		int listLength = 0;
+		listLength = list.Length();		// list: { 2 }				listLength: 1
+		listLength = list2.Length();	// list2: { }				listLength: 0
+		listLength = list3.Length();	// list3: { 1, 2, 3, 4 }	listLength: 4
+		
+		return;
+	}
+
+}
